7d95f2ad2fd21038af5ce6dd54c11f92fc6f5c26
DaviesHub <davieschuks.a@gmail.com>
2022-10-07 19:38:59 +0200

    Store a copy of all  tasks in a dictionary

--- task_manager_modular.py
+++ task_manager_modular.py
@@ -5,7 +5,8 @@ from datetime import date
 
 # The usernames and passwords in the file are separated by a comma, so to validate usernames, commas are removed
 # The usernames and passwords are then stored in a dictionary
-users_dic = {} 
+users_dic = {}
+tasks = [] # Empty list to store all tasks 
 fhand = open("user.txt", "r")
 users = fhand.read()
 users = users.split()
@@ -53,6 +54,8 @@ def reg_user():
 
 def add_task():
     """This function adds new tasks to tasks file"""
+    task_num = 0
+    task_dic = {} # Dictionary to store each part of an individual task
 
     recepient = input("Enter the username of the person to whom the task is assigned: ").lower()
     task_title = input("Enter the title of the task: ")
@@ -63,8 +66,20 @@ def add_task():
     today = date.today()
     today = today.strftime("%d %b %Y") # Formats date to specified format
 
+    task_dic["recepient"] = recepient
+    task_dic["title"] = task_title
+    task_dic["descr"] = task
+    task_dic["d_date"] = due_date
+    task_dic["completion"] = completion
+    task_dic["a_date"] = today
+
     fhand = open("tasks.txt", "a")
-    fhand.write(recepient + ", " + task_title + ", " + task + ", " + today + ", " + due_date + ", " + completion + "\n")
+    for line in fhand:
+        task_num += 1
+    task_dic["task_id"] = task_num
+    tasks.append(task_dic) # Append each task dictionary in the list of tasks
+
+    fhand.write(task_num + ": " + recepient + ", " + task_title + ", " + task + ", " + today + ", " + due_date + ", " + completion + "\n")
     fhand.close()
 
 
@@ -73,19 +88,39 @@ def view_all():
     with open("tasks.txt", "r") as f:
         for line in f:
             line = line.split(", ")
-            print("Task:\t\t" + line[1] + "\nAssigned to:\t" + line[0] + "\nDate assigned:\t" + line[3] +\
-            "\nDue date:\t" + line[4] + "\nTask complete:\t" + line[5] + "Task description: " + line[2] + "\n")
+            print("Task:" + line[0] + "\t\t" + line[2] + "\nAssigned to:\t" + line[1] + "\nDate assigned:\t" + line[4] +\
+            "\nDue date:\t" + line[5] + "\nTask complete:\t" + line[6] + "Task description: " + line[3] + "\n")
 
 
 def view_mine():
     """Function to read tasks assigned to the user who is logged in"""
     # The task's line is splitted into list. The first item (username) is compared to current username and the users tasks displayed
+    task_map = {} # Dictionary to hold tasks assigned to the user and a task number
+    task_list = [] # Empty list to store user's tasks'
+
     with open("tasks.txt", "r") as f:
         for line in f:
             line = line.split(", ")
-            if line[0].lower() == user_name:
-                print("Task:\t\t" + line[1] + "\nAssigned to:\t" + line[0] + "\nDate assigned:\t" + line[3] +\
-                "\nDue date:\t" + line[4] + "\nTask complete:\t" + line[5] + "Task description: " + line[2] + "\n")
+            if line[1].lower() == user_name:
+                task_message = "Task:" + line[0] + "\t\t" + line[2] + "\nAssigned to:\t" + line[1] + "\nDate assigned:\t" + line[4] +\
+                "\nDue date:\t" + line[5] + "\nTask complete:\t" + line[6] + "Task description: " + line[3] + "\n"
+                
+                print(task_message)
+
+
+    # Code to allow a user select a specific task or return to the main menu
+    while True:
+        task_num = input("\nEnter the number of a task to view the task, or enter -1 to return to the main menu:\n")
+        if task_num in task_map.keys():
+            message = f"Task{task_num}: {task_map[task_num]}"
+            print("You have selected the following task:\n", message)
+
+
+            break
+        elif task_num == "-1":
+            pass # Code to return to main menu
+        else:
+            print("Invalid option")
 
 
 def display_stats():
